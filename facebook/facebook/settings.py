cfec46e864b7b29e260bd18ab04aa234acfe7f10
sreekanthdc <=>
2019-07-11 12:25:00 +0530

    initial project

--- facebook/facebook/settings.py
+++ facebook/facebook/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'facebookapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'facebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR+'/facebookapp/template', 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,7 +79,9 @@ WSGI_APPLICATION = 'facebook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydb',
+	    'USER': 'root',
+	    'PASSWORD': '1234',
     }
 }
 
--- facebook/facebookapp/views.py
+++ facebook/facebookapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from . models import *
+# Create your views here.apt
 
-# Create your views here.
+
+def myIndex(request):
+    print("get into index")
+    template = loader.get_template('index.html')
+    context = {"message": "welcome this is my first django application"}
+    return HttpResponse(template.render(context, request))
+
+
+def home(request):
+    print("get into index")
+    template = loader.get_template('home.html')
+    context = {"message": "welcome this is my first django application"}
+    return HttpResponse(template.render(context, request))
+
+
+def checklogin(request):
+   username = request.POST.get('txt_username')
+   password = request.POST.get('txt_password')
+   LoginObj = UserData.objects.filter(userName=username, password=password).exists()
+   if(LoginObj):
+      template = loader.get_template('home.html')
+      return HttpResponse(template.render({}, request))
+   else:
+      return HttpResponse('login failed')
